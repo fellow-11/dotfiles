afc2cb5d03f6b6d771b9aa8cb775438f36252357
fellow-11 <felixoshank@gmail.com>
2026-06-23 14:32:30 -0400

    dotfiles update: 2026-06-23 14:32

--- save.sh
+++ save.sh
@@ -5,31 +5,32 @@ DOTFILES="$HOME/dotfiles"
 echo "Saving dotfiles..."
 
 # ~/.config dirs
-cp -r ~/.config/hypr/*       "$DOTFILES/hypr/"
-cp -r ~/.config/waybar/*     "$DOTFILES/waybar/"
-cp -r ~/.config/walker/*     "$DOTFILES/walker/"
-cp -r ~/.config/fish/*       "$DOTFILES/fish/"
-cp -r ~/.config/theme/*      "$DOTFILES/theme/"
-cp -r ~/.config/btop/*       "$DOTFILES/btop/"
-cp -r ~/.config/nvim/*       "$DOTFILES/nvim/"         2>/dev/null
-cp -r ~/.config/ghostty/*    "$DOTFILES/ghostty/"      2>/dev/null
-cp -r ~/.config/fastfetch/*  "$DOTFILES/fastfetch/"    2>/dev/null
-cp -r ~/.config/gtk-3.0/*    "$DOTFILES/gtk-3.0/"      2>/dev/null
-cp -r ~/.config/gtk-4.0/*    "$DOTFILES/gtk-4.0/"      2>/dev/null
-cp -r ~/.config/wireplumber/* "$DOTFILES/wireplumber/" 2>/dev/null
-cp -r ~/.config/micro/*      "$DOTFILES/micro/"        2>/dev/null
-cp -r ~/.config/wiremix/*    "$DOTFILES/wiremix/"      2>/dev/null
-cp -r ~/.config/magicpods/*  "$DOTFILES/magicpods/"    2>/dev/null
-cp -r ~/.config/mako/*       "$DOTFILES/mako/"         2>/dev/null
-cp -r ~/.config/swayosd/*    "$DOTFILES/swayosd/"      2>/dev/null
-cp -r ~/.config/wlogout/*    "$DOTFILES/wlogout/"      2>/dev/null
+cp -r ~/.config/hypr/*        "$DOTFILES/hypr/"
+cp -r ~/.config/waybar/*      "$DOTFILES/waybar/"
+cp -r ~/.config/walker/*      "$DOTFILES/walker/"
+cp -r ~/.config/fish/*        "$DOTFILES/fish/"
+cp -r ~/.config/theme/*       "$DOTFILES/theme/"
+cp -r ~/.config/btop/*        "$DOTFILES/btop/"
+cp -r ~/.config/nvim/*        "$DOTFILES/nvim/"         2>/dev/null
+cp -r ~/.config/ghostty/*     "$DOTFILES/ghostty/"      2>/dev/null
+cp -r ~/.config/fastfetch/*   "$DOTFILES/fastfetch/"    2>/dev/null
+cp -r ~/.config/gtk-3.0/*     "$DOTFILES/gtk-3.0/"      2>/dev/null
+cp -r ~/.config/gtk-4.0/*     "$DOTFILES/gtk-4.0/"      2>/dev/null
+cp -r ~/.config/wireplumber/* "$DOTFILES/wireplumber/"  2>/dev/null
+cp -r ~/.config/micro/*       "$DOTFILES/micro/"        2>/dev/null
+cp -r ~/.config/wiremix/*     "$DOTFILES/wiremix/"      2>/dev/null
+cp -r ~/.config/magicpods/*   "$DOTFILES/magicpods/"    2>/dev/null
+cp -r ~/.config/mako/*        "$DOTFILES/mako/"         2>/dev/null
+cp -r ~/.config/swayosd/*     "$DOTFILES/swayosd/"      2>/dev/null
+cp -r ~/.config/wlogout/*     "$DOTFILES/wlogout/"      2>/dev/null
+cp -r ~/.config/screensaver/* "$DOTFILES/screensaver/"  2>/dev/null
 
 # Shell
 cp ~/.zshrc "$DOTFILES/zsh/"
 
 # ~/.local/
-cp -r ~/.local/bin/* "$DOTFILES/scripts/" 2>/dev/null
-cp -r ~/.local/share/fonts/* "$DOTFILES/fonts/" 2>/dev/null
+cp -r ~/.local/bin/*         "$DOTFILES/scripts/" 2>/dev/null
+cp -r ~/.local/share/fonts/* "$DOTFILES/fonts/"   2>/dev/null
 
 # System
 cp /usr/local/bin/t2-suspend.sh      "$DOTFILES/system/"
@@ -37,7 +38,7 @@ cp /usr/local/bin/t2-resume.sh       "$DOTFILES/system/"
 cp /etc/t2-suspend-fix/hardware.conf "$DOTFILES/system/"
 sudo cp /boot/limine.conf            "$DOTFILES/system/"
 sudo chown "$USER:$USER"             "$DOTFILES/system/limine.conf"
-cp -r ~/.config/pipewire/*          "$DOTFILES/pipewire/"
+cp -r ~/.config/pipewire/*           "$DOTFILES/pipewire/"
 
 # Systemd
 sudo cp /etc/systemd/sleep.conf                                "$DOTFILES/systemd/"
@@ -47,7 +48,7 @@ sudo cp /etc/systemd/journald.conf                             "$DOTFILES/system
 sudo cp /etc/systemd/system/t2-suspend.service                 "$DOTFILES/systemd/"
 sudo cp /etc/systemd/system/t2-resume.service                  "$DOTFILES/systemd/"
 sudo cp /etc/systemd/system/t2-fix-kbd-backlight.service       "$DOTFILES/systemd/"
-sudo chown -R "$USER:$USER" "$DOTFILES/systemd/"
+sudo chown -R "$USER:$USER"                                    "$DOTFILES/systemd/"
 
 # Wallpapers
 cp -r ~/.local/share/wallpapers/custom-wallpapers/* "$DOTFILES/wallpapers/" 2>/dev/null
